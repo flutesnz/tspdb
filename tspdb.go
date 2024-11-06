89c49d062f34b081faabf2ae4fe59104cd48dcac
flutesnz <thomas@flutes.nz>
2024-11-07 01:30:54 +1300

    Update tspdb.go

--- tspdb.go
+++ tspdb.go
@@ -26,5 +26,8 @@ func checkErrorFatal(err error, exit bool) {
 func InitDatabaseConnection(dbFile string) (*sql.DB, error) {
 	db, err := sql.Open("sqlite3", dbFile)
 	checkError(err)
+
+	 _, err = db.Exec("CREATE TABLE IF NOT EXISTS example (id INTEGER PRIMARY KEY, name TEXT)") checkError(err)
+	checkError(err)
 	return db, nil
 }
